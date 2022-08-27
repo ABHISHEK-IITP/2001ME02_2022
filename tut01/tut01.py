9f8fa0e82c88db962d6a476d1413050bf2035e28
ABHISHEK-IITP <abhi9813293405@gmail.com>
2022-08-27 22:45:35 +0530

    tut1 solution

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,1 +1,16 @@
-print("hello world")
+
+number=1
+
+number = int(input("Enter a number:"))
+
+fact = 1
+
+if number<0:
+    print("Fctorial does not exist")
+elif number==0:
+    print("Factorial of 0 = 1")  
+else:
+    for i in range (1,number+1):
+      fact = fact*i
+    
+print("Factorial of " ,number ,"is" ,fact,)
