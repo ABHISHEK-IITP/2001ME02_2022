3ab7d42dcd21a22ff20f9864df060c5d90bc9857
ABHISHEK-IITP <abhi9813293405@gmail.com>
2022-11-27 11:15:20 +0530

    getting octant

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -1,34 +1,105 @@
-
 from datetime import datetime
+from openpyxl.styles import PatternFill
+from openpyxl.formatting.rule import CellIsRule
+from openpyxl.styles.borders import Border, Side
 start_time = datetime.now()
 
-#Help
+# Help
+
+
 def octant_analysis(mod=5000):
-	pass
+    from openpyxl import load_workbook
+    import os
+    import numpy as np
+    from operator import itemgetter
 
-##Read all the excel files in a batch format from the input/ folder. Only xlsx to be allowed
-##Save all the excel files in a the output/ folder. Only xlsx to be allowed
-## output filename = input_filename[_octant_analysis_mod_5000].xlsx , ie, append _octant_analysis_mod_5000 to the original filename. 
+    os.system('cls')
+    curr = os.getcwd()  
+    if os.path.exists("output"):
+        for f in os.listdir("output"):  
+            os.remove(os.path.join("output", f))
+        os.rmdir("output")  # delete output folder
+    os.mkdir(curr.replace('\\', '/')+"/output/")  # make output folder
+    os.chdir("input")
+    fil = os. listdir()  # storing all file name of input folder in list
+    os.chdir(curr)
+    for list in fil:
+        print(list)
+        os.chdir("input")  # taking input
+        wb = load_workbook(list)
+        sheet = wb.active
 
-###Code
+        ######### making  new lists ##########
+        time = []  
+        v = []   
+        u = []   
+        w = []   
 
-from platform import python_version
-ver = python_version()
+        # making  lists for difference
+        vd = []    
+        ud = []    
+        wd = []    
+        oct = []    # make a list for octant stori
+        wb = load_workbook(list)
 
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+        sheet = wb.active
+        n = sheet.max_row  
+        for r in range(2, n+1):       
+            time.append(sheet.cell(row=r, column=1).value)
+            u.append(sheet.cell(row=r, column=2).value)
+            # making of w list from given data
+            v.append(sheet.cell(row=r, column=3).value)
+            w.append(sheet.cell(row=r, column=4).value)
+        
+        ########### Calculating mean v,u,w
+        v_mean = np.mean(v, dtype=np.float64)  
+        u_mean = np.mean(u, dtype=np.float64) 
+        w_mean = np.mean(w, dtype=np.float64) 
 
+        for r in range(2, n+1):
+            ud.append(float(sheet.cell(row=r, column=2).value) -
+                      u_mean)  
+            vd.append(float(sheet.cell(row=r, column=3).value) -
+                      v_mean)  
+            wd.append(float(sheet.cell(row=r, column=4).value) -
+                      w_mean)  
+        tu3 = []
 
-mod=5000
-octant_analysis(mod)
+        ######### Getting octant     
+        for i in range(0, n-1):  
+            if ((ud[i] >= 0) and (vd[i] >= 0) and (wd[i] >= 0)):
+                oct.append(1)
+            elif ((ud[i] >= 0) and (vd[i] >= 0) and (wd[i] < 0)):
+                oct.append(-1)
+            elif ((ud[i] < 0) and (vd[i] >= 0) and (wd[i] >= 0)):
+                oct.append(2)
+            elif ((ud[i] < 0) and (vd[i] >= 0) and (wd[i] < 0)):
+                oct.append(-2)
+            elif ((ud[i] >= 0) and (vd[i] < 0) and (wd[i] >= 0)):
+                oct.append(4)
+            elif ((ud[i] >= 0) and (vd[i] < 0) and (wd[i] < 0)):
+                oct.append(-4)
+            elif ((ud[i] < 0) and (vd[i] < 0) and (wd[i] >= 0)):
+                oct.append(3)
+            else:
+                oct.append(-3)
+        
+        ######## Declaring variables for storing the longest subsequence value of octant -1
+        lon_s1 = 0  
+        lon_s2 = 0 
+        lon_s3 = 0 
+        lon_s4 = 0  
+        lon_s5 = 0 
+        lon_s6 = 0  
+        lon_s7 = 0 
+        lon_s8 = 0  
 
-
-
-
-
-
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+        ########### Declaring variables giving the longest subsequence length for octants
+        a = 0  
+        b = 0  
+        c = 0 
+        d = 0  
+        e = 0 
+        f = 0  
+        g = 0 
+        h = 0  
